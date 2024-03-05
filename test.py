49e7a4433106a810ded7dd9b5bdbc0ef2a79d265
primolechuga <juanestebaca123@gmail.com>
2024-03-04 19:40:42 -0500

    reftest

--- test.py
+++ test.py
@@ -18,11 +18,11 @@ if not os.path.exists("countries.csv"):
 dataframe = pd.read_csv("countries.csv")
 dataframe = dataframe.set_index("countries")
 
-assert dataframe["United States of America"] == 579
-assert dataframe["China"] == 273
-assert dataframe["India"] == 174
-assert dataframe["United Kingdom"] == 173
-assert dataframe["Italy"] == 112
+assert dataframe.loc["United States of America"].values[0] == 579
+assert dataframe.loc["China"].values[0] == 273
+assert dataframe.loc["India"].values[0] == 174
+assert dataframe.loc["United Kingdom"].values[0] == 173
+assert dataframe.loc["Italy"].values[0] == 112
 
 if not os.path.exists("map.html"):
     raise FileNotFoundError("File 'map.html' not found")
